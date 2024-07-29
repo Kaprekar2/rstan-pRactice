41305f9882865635a07c747cb7273773ff349e12
Riki TAKARA <rikinine97@gmail.com>
2024-07-29 22:46:27 +0900

    out some result graphs

--- chap4.R
+++ chap4.R
@@ -1,4 +1,4 @@
-pacman::p_load(rstan, tidyverse, ggplot2)
+pacman::p_load(rstan, tidyverse, ggplot2, ggmcmc)
 
 
 salary <- read.table("./data-salary.txt", header = TRUE, sep = ",")
@@ -18,3 +18,17 @@ pred_95 <- predict(res_lm, year_new, interval = "prediction", level = 0.95)
 
 dat <- list(N = nrow(salary), X = salary$X, Y = salary$Y)
 fit <- stan(file = "./model45.stan", data = dat, seed = 1234)
+save.image(file = "./output/result_model45.RData")
+
+load("./output/result_model45.RData")
+write.table(data.frame(summary(fit)$summary, check.names=FALSE),
+            file='output/fit-summary.csv', sep=',', quote=TRUE, col.names=NA)
+
+ggmcmc(ggs(fit, inc_warmup = TRUE, stan_include_auxiliar = TRUE),
+       file = "output/fit-traceplot.pdf", plot = "traceplot")
+
+
+# plotを指定せずに描くとtrace plotだけでなくサンプルから算出された密度関数や事後平均
+# の推移、自己相関などが図示される
+
+ggmcmc(ggs(fit), file='output/fit-ggmcmc.pdf')
